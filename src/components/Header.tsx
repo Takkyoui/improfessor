8877a356b9854d81994357a0606387db48de8466
Takkyoui <takelucky5659@gmail.com>
2025-10-15 01:47:45 +0900

    UI 개선: Header 컴포넌트의 SVG 마스크 속성을 업데이트하여 코드 일관성을 높임. maskUnits 속성을 수정하여 가독성을 향상시킴.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -62,7 +62,7 @@ export default function Header() {
             className="text-white hover:opacity-70 transition-opacity"
           >
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-              <mask id="mask0_227_52" style={{maskType:'alpha', maskUnits:'userSpaceOnUse', x:'0', y:'0', width:'24', height:'24'}}>
+              <mask id="mask0_227_52" maskUnits="userSpaceOnUse" x="0" y="0" width="24" height="24" style={{maskType:'alpha'}}>
                 <rect width="24" height="24" fill="#D9D9D9"/>
               </mask>
               <g mask="url(#mask0_227_52)">
@@ -94,7 +94,7 @@ export default function Header() {
                     </span>
                   )}
                   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                    <mask id="mask0_227_59" style={{maskType:'alpha', maskUnits:'userSpaceOnUse', x:'0', y:'0', width:'24', height:'24'}}>
+                    <mask id="mask0_227_59" maskUnits="userSpaceOnUse" x="0" y="0" width="24" height="24" style={{maskType:'alpha'}}>
                       <rect width="24" height="24" fill="#D9D9D9"/>
                     </mask>
                     <g mask="url(#mask0_227_59)">
